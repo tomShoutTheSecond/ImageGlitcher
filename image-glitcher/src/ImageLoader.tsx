8b264a8b887f41b139e015c35b36a974322a0243
TomDawes <tom.dawes@shout-app.com>
2020-06-22 01:06:35 +0100

    Start to implement input image converter

--- image-glitcher/src/ImageLoader.tsx
+++ image-glitcher/src/ImageLoader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { State } from './App';
 import { Colors } from './Colors';
 import { Styles } from './Styles';
+import Jimp from 'jimp';
 
 export class ImageLoader extends React.Component
 {
@@ -31,21 +32,20 @@ export class ImageLoader extends React.Component
                 <h1 style={Styles.h1Style}>Load Image</h1>
                 <p>.bmp files only</p>
                 <div style={innerContainerStyle}>
-                    <input ref="fileInput" type="file" id="files" name="file" onChange={() => this.loadImage()}/>
+                    <input ref="fileInput" type="file" id="files" name="file" onChange={() => this.loadImageFromFile()}/>
                 </div>
                 <img src={this.state.previewUrl} style={Styles.imageStyle}/>
             </div>
         );
     }
 
-    loadImage()
+    loadImageFromFile()
     {
         let fileInput = this.refs.fileInput as HTMLInputElement;
         let imageFile = fileInput.files![0];
         
         let fileReader = new FileReader();
         fileReader.readAsArrayBuffer(imageFile);
-
         fileReader.onloadend = () =>
         {
             //get data from file
@@ -58,8 +58,54 @@ export class ImageLoader extends React.Component
 
             fileReader.onloadend = () =>
             {
-                this.setState({previewUrl: fileReader.result});
+                let originalImageUrl = fileReader.result as string;
+
+                if(!imageFile.name.endsWith(".bmp"))
+                {
+                    this.convertImage(originalImageUrl);
+                }
+
+                this.setState({ previewUrl: originalImageUrl });
             }
         }
+    }
+
+    loadConvertedImage(imageBlob : Blob)
+    {
+        let fileReader = new FileReader();
+
+        //set image data
+        fileReader.onloadend = () =>
+        {
+            let convertedImageData = fileReader.result as Uint8Array;
+            State.setImageData(convertedImageData);
+
+            console.log("Converted image was loaded");
+        }
+        fileReader.readAsArrayBuffer(imageBlob);
+    }
+
+    convertImage(imageUrl : string)
+    {
+        Jimp.read(imageUrl, (err, image) =>
+        {
+            if(err) 
+            {
+                console.log(err);
+            } 
+            else 
+            {
+                image.getBuffer(Jimp.MIME_BMP, (error, data) => 
+                {
+                    let convertedImageBlob = this.bufferToBlob(data, "image/bmp");
+                    this.loadConvertedImage(convertedImageBlob)
+                });
+            }
+        });
+    }
+
+    bufferToBlob(buffer : Buffer, contentType : string)
+    {
+        return new Blob([ buffer ], { type: contentType });
     }
 }
