e9db6fe380663e2a0a1372950948528c79235436
tomDawesShout <tom.dawes@shout-app.com>
2020-10-25 19:17:40 +0000

    Clear commented code

--- image-glitcher/src/AudioProcessorWindow.tsx
+++ image-glitcher/src/AudioProcessorWindow.tsx
@@ -1,12 +1,7 @@
-import React, { createRef, RefObject } from 'react';
-import { State, KeyFrame } from './App';
+import React from 'react';
+import { State } from './App';
 import { Colors } from './Colors';
 import { Styles } from './Styles';
-import ReactDOM from 'react-dom';
-import JSZip from 'jszip';
-import { saveAs } from 'file-saver';
-import { Util } from './Util';
-import { FrameHolder } from './FrameHolder';
 import { IconButton } from './IconButton';
 import { AudioProcessor } from './AudioProcessor';
 import { Waveform } from './Waveform';
@@ -112,35 +107,10 @@ export class AudioProcessorWindow extends React.Component<AudioProcessorWindowPr
         console.log("bufferVolumeEnvelope", bufferVolumeEnvelope)
         State.setAudioEnvelope(sourceIndex, bufferVolumeEnvelope, filename);
     }
-/*
-    changeSmoothing(key : number, event : any)
-    {
-        let smoothing = (event.target as HTMLInputElement)?.valueAsNumber;
-        if (smoothing == undefined) return;
 
-        let smoothings = this.state.smoothings;
-        smoothings[key] = Math.round(smoothing);
-        this.setState({ smoothings : smoothings });
-
-        console.log("smoothing changed to ", smoothing)
-    }
-
-    changeFramesPerSecond(index : number)
-    {
-        let fpsInput = this.fpsInputs[index];
-        if(fpsInput == null) return;
-        
-        let fps = fpsInput.valueAsNumber;
-        if (fps == undefined) return;
-
-        this.setState({ framesPerSecond: Math.round(fps) });
-    }
-*/
     async loadAudioFromFile(key : number, event : any)
     {
-        //let fileInput = this.fileInput.current as HTMLInputElement;
         let audioFile = this.fileInputs[key]?.files![0];
-        //let audioFile = (event.target as HTMLInputElement).files![0];
 
         if(!audioFile)
         {
@@ -152,45 +122,5 @@ export class AudioProcessorWindow extends React.Component<AudioProcessorWindowPr
         audioFiles[key] = audioFile;
         console.log(audioFiles)
         this.setState({ audioFiles: audioFiles });
-
-        //let audioIsWav = audioFile.name.endsWith(".wav");
-        
-        //let fileReader = new FileReader();
-
-        /*
-        fileReader.readAsArrayBuffer(audioFile);
-        fileReader.onloadend = () =>
-        {
-            //get data from file (if bitmap was supplied)
-            if(audioIsWav)
-            {
-                this.audioProcessor.decodeFile()
-
-                //let result = fileReader.result as ArrayBuffer;
-                //let rawData = new Uint8Array(result);
-                //console.log("audio data:", rawData);
-                //State.setImageData(rawData);
-            }
-            /*
-            //put preview in component
-            fileReader.readAsDataURL(audioFile);
-
-            fileReader.onloadend = () =>
-            {
-                let originalImageUrl = fileReader.result as string;
-
-                if(!audioIsWav)
-                {
-                    this.setState({ isConverting: true });
-                    this.convertImage(originalImageUrl);
-                }
-                else
-                {
-                    this.setState({ previewUrl: originalImageUrl });
-                    console.log("Original image was loaded");
-                }
-            }
-            
-        }*/
     }
 }
