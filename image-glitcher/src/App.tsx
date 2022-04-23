f65a9ae3ad52ca0e45fcb8cfe7842e62f773df42
tomDawesShout <tom.dawes@shout-app.com>
2022-04-23 22:40:40 +0100

    Add task to todo list

--- image-glitcher/src/App.tsx
+++ image-glitcher/src/App.tsx
@@ -320,6 +320,7 @@ export class TransitionFramebank
 // x reuse split zip function from the old FrameInspector frame sequence download, to avoid memory overload when downloading timeline frames
 // x figure out which frames should be omitted and share the logic between GIF maker and timeline download
 // x calculate correct firstFrameIndex in TransitionWindow to enable rendering sequences of transitions in the timeline
+// - make generate keyframe variant button
 // - add a dispose GIF animation button to free up some memory
 // - add a memory watcher window with rolling stats
 // - experiment with pixel sorting algorithms
